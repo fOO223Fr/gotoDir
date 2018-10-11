cd34d8c653a16e1386593c7ba4efd95f48d8dfb6
garlicFrancium <suvroindia.123@gmail.com>
2018-10-11 15:15:52 -0400

    Updated watchAndRsync

--- watchAndSync.sh
+++ watchAndSync.sh
@@ -1,7 +1,21 @@
-echo "Type the directory to watch and rsync in Home directory"
+echo "Type the Microservice directory to watch and rsync, followed by [Enter]"
 read directory
+echo "Searching for $directory anywhere inside $HOME"
 directory_path=$(find ~/ -name $directory -type d -print)
-echo "$directory_path"
+echo "Found path for $directory which is: $directory_path"
+echo "Enter the Namespace were to sync, example: 'platform' or 'security' or etc"
+read namespace
+echo "Enter role name"
+read role_name
 while inotifywait -r -e modify,create,delete $directory_path; do
-    rsync -avu --delete $directory_path/src/main/deployment/ansible/setup-generic-nginx-ms/tasks/ /etc/ansible_guest/roles/setup-generic-nginx-ms/tasks/
+	# For playbook file
+    rsync -vu $directory_path/src/main/deployment/ansible/ /etc/ansible_guest/
+    # For tasks folder
+    rsync -avu --delete $directory_path/src/main/deployment/ansible/$role_name/tasks/ /etc/ansible_guest/roles/$namespace/$role_name/tasks/
+    # For config.yml file 
+    rsync -vu $directory_path/src/main/deployment/ansible/config.yml /etc/ansible_guest/roles/$namespace/$role_name/files/config.yml
+    # For docker folder
+    rsync -avu --delete $directory_path/src/main/deployment/docker/ /etc/ansible_guest/roles/$namespace/$role_name/files/docker/
+    # For Kubernetes folder
+    rsync -avu --delete $directory_path/src/main/deployment/kubernetes/ /etc/ansible_guest/roles/$namespace/$role_name/templates/kubernetes/
 done
