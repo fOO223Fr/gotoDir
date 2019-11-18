3c322f158ccd7c6b3b1cf1e45cf2400e41ad63b6
Suvro <Suvrojeet.Ghosh@kandy.io>
2019-11-18 12:52:39 -0500

    removed temp files

--- watchAndSync_BASE_6054.sh
+++
@@ -1,7 +0,0 @@
-echo "Type the directory to watch and rsync in Home directory"
-read directory
-directory_path=$(find ~/ -name $directory -type d -print)
-echo "$directory_path"
-while inotifywait -r -e modify,create,delete $directory_path; do
-    rsync -avu --delete $directory_path/src/main/deployment/ansible/setup-generic-nginx-ms/tasks/ /etc/ansible_guest/roles/setup-generic-nginx-ms/tasks/
-done
